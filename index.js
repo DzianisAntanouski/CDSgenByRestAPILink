c48985f461e817af3fd6eb2b52fb50ee30a376bb
Dzianis Antanouski <dzianis.antanouski@LeverX.local>
2023-02-10 01:13:44 +0300

    ver 2

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const npmInstall = require("./module/npmInstall");
 const initializeCds = require("./module/initializeCds")
 const addFetchNode = require("./module/addFetchNode")
 
-const initialization = async (url, sName) => {
+const data = [
+    {
+        name: "users",
+        url: "https://jsonplaceholder.typicode.com/users"
+    },
+    {
+        name: "posts",
+        url: "https://jsonplaceholder.typicode.com/posts"
+    }
+]
+
+const initialization = async (data) => {
     await initializeCds();
     console.log("------------cds init completed");
     await addFetchNode();
     console.log("------------fetch installed");
     await npmInstall();
     console.log("------------npm i completed");
-    await require("./module/fetchAndSaveData")(url, sName);
-    console.log("------------dir created");
+    data.forEach(async (element) => {
+        await require("./module/fetchAndSaveData")(element);
+        console.log("------------dir created");
+    });
+    
 };
+
 
 initialization("https://jsonplaceholder.typicode.com/users", "users");
--- module/fetchAndSaveData.js
+++ module/fetchAndSaveData.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const fetch = require("node-fetch");
 
-module.exports = async function fetchAndSaveData(url, sName) {
+module.exports = async function fetchAndSaveData(dataObj) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(dataObj.url);
         const data = await response.json();
         if (!Array.isArray(data)) {
             console.error(`Error: Response is not an array. Response: ${data}`);
@@ -12,7 +12,7 @@ module.exports = async function fetchAndSaveData(url, sName) {
         }
 
         const dataPath = path.join(__dirname, "data");
-        const dataFile = path.join(dataPath, `${sName}.json`);
+        const dataFile = path.join(dataPath, `${dataObj.name}.json`);
 
         if (!fs.existsSync(dataPath)) {
             fs.mkdirSync(dataPath);
